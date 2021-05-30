1c7e11e6af1d162c60aa32c72070f625d978fe83
Aviraj Newatia <projectavi.exe@gmail.com>
2021-05-30 19:12:51 +0530

    fixed port from 5000 to 3000

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
