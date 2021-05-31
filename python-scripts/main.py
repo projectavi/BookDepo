0ef204af1b18edcff5ff89dac013e4af8774c2b0
Aviraj Newatia <projectavi.exe@gmail.com>
2021-05-31 15:40:59 +0530

    buildpacks

--- python-scripts/main.py
+++ python-scripts/main.py
@@ -25,7 +25,7 @@ def onelib_sel(name):
     options.add_argument('--incognito')
     options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
-    #options.add_argument('--headless')
+    options.add_argument('--headless')
     #driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
     options.binary_location = GOOGLE_CHROME_PATH
     driver = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=options)
